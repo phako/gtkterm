649e3e2ee2f1b711b792c3496d0d78f293e3aaab
Jens Georg <jens.georg@desy.de>
2019-08-05 14:05:08 +0200

    Migrate config to new place

--- src/gtkterm.c
+++ src/gtkterm.c
@@ -32,37 +32,82 @@
 #include <config.h>
 #include <glib/gi18n.h>
 
-void migrate_config ()
+static void migrate_config (void)
 {
     char *old_config_path = NULL;
-    GFile *old_config_file = NULL;
+    char *new_config_path = NULL;
+    GFile *file = NULL;
+    GFile *new_file = NULL;
     GDataInputStream *stream = NULL;
-    GInputStream *input_stream = NULL;
+    GFileInputStream *input_stream = NULL;
     GError *error = NULL;
     char *line = NULL;
+    int macro_counter = 0;
+    const char macro_regex[] = "^[[:space:]]*macros[[:space:]]*=";
+    GRegex *re = g_regex_new (macro_regex, 0, 0, NULL);
+    GString *config = g_string_new ("");
+    gsize length = 0;
 
-    old_config_file = g_build_filename (g_get_home_dir (), ".gtktermrc", NULL);
-    g_clear_pointer (&old_config_path);
+    old_config_path = g_build_filename (g_get_home_dir (), ".gtktermrc", NULL);
     file = g_file_new_for_commandline_arg (old_config_path);
 
-    if (!g_file_query_exists (file)) {
-        return;
+    new_config_path = g_build_filename (g_get_user_config_dir (), "gtktermrc", NULL);
+    new_file = g_file_new_for_commandline_arg (new_config_path);
+
+    // Do nothing if there is no old file or already a new file. To prevent
+    // overwriting a previously converted file
+    if (!g_file_query_exists (file, NULL) || g_file_query_exists (new_file, NULL)) {
+        goto out;
     }
 
     input_stream = g_file_read (file, NULL, &error);
-    stream = g_data_input_stream_new (input_stream);
+    stream = g_data_input_stream_new (G_INPUT_STREAM (input_stream));
     for (line = g_data_input_stream_read_line (stream, &length, NULL, &error);
          line != NULL;
          line = g_data_input_stream_read_line (stream, &length, NULL, &error)) {
+        if (g_str_has_prefix (line, "["))
+            macro_counter = 0;
 
+        if (g_regex_match (re, line, 0, NULL)) {
+            char *prefix = NULL;
+            char *old_line = NULL;
+
+            prefix = g_strdup_printf ("macros%d=", macro_counter++);
+            old_line = line;
+
+            line = g_regex_replace_literal (re, line, -1, 0, prefix, 0, &error);
+            if (line != old_line)
+                g_free (old_line);
+
+            g_free (prefix);
+        }
+
+        g_string_append (config, line);
+        g_string_append_c (config, '\n');
     }
+
+    g_file_set_contents (new_config_path, config->str, config->len, &error);
+
+    g_input_stream_close (G_INPUT_STREAM (stream), NULL, &error);
+    g_file_delete (file, NULL, &error);
+
+out:
+    g_clear_object (&stream);
+    g_clear_object (&input_stream);
+    g_clear_pointer (&old_config_path, g_free);
+    g_clear_object (&file);
+    g_clear_pointer (&re, g_regex_unref);
+    g_clear_pointer (&new_config_path, g_free);
+    g_clear_object (&new_file);
 }
 
 int main(int argc, char *argv[])
 {
   gchar *message;
 
-  config_file = g_strdup_printf("%s/.gtktermrc", getenv("HOME"));
+  migrate_config ();
+
+  config_file = g_build_filename (g_get_user_config_dir(), "gtktermrc", NULL);
 
   bindtextdomain(PACKAGE, LOCALEDIR);
   bind_textdomain_codeset(PACKAGE, "UTF-8");
